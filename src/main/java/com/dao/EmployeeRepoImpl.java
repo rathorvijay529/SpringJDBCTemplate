9f95d7bace11e2941bc39129e20030571b7b9f17
vijay <vijay@vijay-PC>
2019-06-22 13:57:24 -1200

    New changs according to Class project in DAo layer

--- src/main/java/com/dao/EmployeeRepoImpl.java
+++ src/main/java/com/dao/EmployeeRepoImpl.java
@@ -4,112 +4,154 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
-
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.PreparedStatementSetter;
 import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.entity.EmployeeEntity;
-import com.properties.Properties;
+import com.model.Employee;
+import com.properties.Queries;
 
 @Repository
-public class EmployeeRepoImpl implements Employeerepo {
-
-	Logger logger = Logger.getLogger(EmployeeRepoImpl.class);
-
-	@Autowired
-	private JdbcTemplate jdbcTemplate;
+public class EmployeeRepoImplement implements EmployeeRepo {
+	private static final Logger logger = Logger
+			.getLogger(EmployeeRepoImplement.class);
 
 	@Autowired
-	private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+	private JdbcTemplate jdbctemplate;
 
-	@Override
-	public Integer save(EmployeeEntity employeeEntity) {
-		logger.info("Insertion is hppning in DAO layer and Method::save is invoked");
-		return jdbcTemplate.update(Properties.queryForInsertEmployee, PreparedStatement -> {
-			PreparedStatement.setInt(1, employeeEntity.getEmployeeId());
-			PreparedStatement.setString(2, employeeEntity.getFname());
-			PreparedStatement.setString(3, employeeEntity.getLname());
-			PreparedStatement.setInt(4, employeeEntity.getAge());
-			PreparedStatement.setString(5, employeeEntity.getBranchName());
+	public Employee getEmployee(final Integer id) {
+		try {
+			logger.info("Repository Layer Invoked::EmployeeRepoImplement {}");
+			logger.info("Retriving  the Employee is processing method name::getEmployee");
+			logger.info("Argument::" + "id");
+			return jdbctemplate.query(Queries.queryForGettingEmployee,
+					new PreparedStatementSetter() {
 
-		});
+						public void setValues(PreparedStatement ps)
+								throws SQLException {
+							ps.setInt(1, id);
+						}
 
-	}
+					}, new RowMapper<Employee>() {
 
-	@Override
-	public Integer saveWithNamedParameter(EmployeeEntity employeeEntity) {
-		logger.info("Insertion is hppning in DAO layer and Method::save is invoked");
-		return namedParameterJdbcTemplate.update(Properties.queryForInsertEmployeeWithNamedParaMeter,
-				new MapSqlParameterSource().addValue("id", employeeEntity.getEmployeeId())
-						.addValue("fname", employeeEntity.getFname()).addValue("lname", employeeEntity.getLname())
-						.addValue("age", employeeEntity.getAge()).addValue("branchName", employeeEntity.getBranchName())
+						public Employee mapRow(ResultSet resultSet, int rowNum)
+								throws SQLException {
+							Employee employee = new Employee();
+							employee.setId(resultSet.getInt(1));
+							employee.setAge(resultSet.getInt(2));
+							employee.setFirstName(resultSet.getString(3));
+							employee.setLastName(resultSet.getString(4));
+							employee.setAddress(resultSet.getString(5));
+							employee.setBloodGroup(resultSet.getString(6));
+							employee.setDepartmentName(resultSet.getString(7));
+							employee.setEmployeeType(resultSet.getString(8));
+							return employee;
+						}
 
-		);
-
-	}
-
-	@Override
-	public List<EmployeeEntity> getListOfEmployee() {
-		logger.info("Insertion is hppning in DAO layer and Method::save is invoked");
-		return jdbcTemplate.query(Properties.queryForGettingAllEmployee, new RowMapper<EmployeeEntity>() {
-			@Override
-			public EmployeeEntity mapRow(ResultSet rs, int rowNum) throws SQLException {
-				EmployeeEntity employeeEntity = new EmployeeEntity();
-				employeeEntity.setEmployeeId(rs.getInt(1));
-				employeeEntity.setFname(rs.getString(2));
-				employeeEntity.setLname(rs.getString(3));
-				return employeeEntity;
-			}
-		});
-	}
-
-	@Override
-	public EmployeeEntity getEmployee(Integer empId) {
-		logger.info("getting information in DAO layer and Method::getEmployee is invoked");
-		return jdbcTemplate.query(Properties.queryForGettingEmployee, new PreparedStatementSetter() {
-			@Override
-			public void setValues(PreparedStatement preparedStatement) throws SQLException {
-				preparedStatement.setInt(1, empId);
-			}
-
-		}, new RowMapper<EmployeeEntity>() {
-			@Override
-			public EmployeeEntity mapRow(ResultSet rs, int rowNum) throws SQLException {
-				EmployeeEntity employeeEntity = new EmployeeEntity();
-				employeeEntity.setEmployeeId(rs.getInt(1));
-				employeeEntity.setFname(rs.getString(2));
-				employeeEntity.setLname(rs.getString(3));
-				return employeeEntity;
-			}
-		}).get(0);
-	}
-
-	@Override
-	public Boolean deleteEmployee(Integer empId) {
-		logger.info(
-				"Deleting information in DAO layer and Method::deleteEmployee is invoked and parameter is " + empId);
-
-		jdbcTemplate.update(Properties.queryForDeleteEmployee, new PreparedStatementSetter() {
-
-			@Override
-			public void setValues(PreparedStatement ps) throws SQLException {
-				ps.setInt(1, empId);
-			}
-		});
-		return true;
+					}).get(0);
+		} catch (EmptyResultDataAccessException e) {
+			return null;
+		}
 
 	}
 
-	@Override
-	public EmployeeEntity updateEmployee(Integer empId) {
+	public List<Employee> getEmployees() {
+		logger.info("Repository Layer Invoked::EmployeeRepoImplement");
+		logger.info("Retriving  the Employee is processing method name::getEmployees");
+		logger.info("Argument::" + "");
+		return jdbctemplate.query(Queries.queryForGettingAllEmployee,
+				new RowMapper<Employee>() {
 
-		return null;
+					public Employee mapRow(ResultSet resultSet, int rowNum)
+							throws SQLException {
+						Employee employee = new Employee();
+						employee.setId(resultSet.getInt(1));
+						employee.setAge(resultSet.getInt(2));
+						employee.setFirstName(resultSet.getString(3));
+						employee.setLastName(resultSet.getString(4));
+						employee.setAddress(resultSet.getString(5));
+						employee.setBloodGroup(resultSet.getString(6));
+						employee.setDepartmentName(resultSet.getString(7));
+						employee.setEmployeeType(resultSet.getString(8));
+						return employee;
+					}
+
+				});
+	}
+
+	public void delete(final int id) {
+
+		try {
+			logger.info("Repository Layer Invoked::EmployeeRepoImplement");
+			logger.info("Deleting  the Employee is processing method name::delete");
+			logger.info("Argument::" + "id");
+			jdbctemplate.update(Queries.queryForDeleteEmployee,
+					new PreparedStatementSetter() {
+
+						public void setValues(PreparedStatement ps)
+								throws SQLException {
+							ps.setInt(1, id);
+						}
+
+					});
+			logger.info("Deleted");
+		} catch (EmptyResultDataAccessException e) {
+
+		}
+	}
+
+	public void save(final EmployeeEntity emp) {
+		logger.info("Repository Layer Invoked::EmployeeRepoImplement");
+		logger.info("Saving the Employee is processing method name::save");
+		logger.info("Argument::" + emp);
+		jdbctemplate.update(Queries.INSERT_EMPLOYEES,
+				new PreparedStatementSetter() {
+
+					public void setValues(PreparedStatement preparedstatement)
+							throws SQLException {
+						preparedstatement.setInt(1, emp.getId());
+						preparedstatement.setInt(2, emp.getAge());
+						preparedstatement.setString(3, emp.getFirstName());
+						preparedstatement.setString(4, emp.getLastName());
+						preparedstatement.setString(5, emp.getAddress());
+						preparedstatement.setString(6, emp.getBloodGroup());
+						preparedstatement.setString(7, emp.getDepartmentName());
+						preparedstatement.setString(8, emp.getEmployeeType());
+					}
+
+				});
+		logger.info("Data Inserted Success");
+
+	}
+
+	public String updateEmployee(final EmployeeEntity emp) {
+		logger.info("Repository Layer Invoked::EmployeeRepoImplement");
+		logger.info("Updating the Employee is processing method name::updateEmployee");
+		logger.info("Argument::" + emp);
+		jdbctemplate.update(Queries.queryForUpdateEmployee,
+				new PreparedStatementSetter() {
+
+					public void setValues(PreparedStatement preparedstatement)
+							throws SQLException {
+						preparedstatement.setInt(8, emp.getId());
+						preparedstatement.setInt(2, emp.getAge());
+						preparedstatement.setString(1, emp.getFirstName());
+						preparedstatement.setString(3, emp.getLastName());
+						preparedstatement.setString(4, emp.getAddress());
+						preparedstatement.setString(5, emp.getBloodGroup());
+						preparedstatement.setString(6, emp.getDepartmentName());
+						preparedstatement.setString(7, emp.getEmployeeType());
+					}
+
+				});
+		logger.info("Data Updated Successfully");
+
+		return "updated";
 	}
 
 }
