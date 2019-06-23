dab69e34070aa54102bb37d7fb87d9fba5a08e04
rizwan <rizwan@XE-T-RIZWAN.XEBIAINDIA.LOCAL>
2019-06-23 19:00:39 +0530

    Implemented getEmployeeById and SaveEmployee in Hibernatecontroller

--- src/main/java/com/controller/ControllerForHibernateOperation.java
+++ src/main/java/com/controller/ControllerForHibernateOperation.java
@@ -4,11 +4,15 @@ import org.apache.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.customException.MissingParameterInThePayLoad;
+import com.entity.EmployeeEntity;
 import com.entity.ErrorResponse;
+import com.entity.SuccessResponse;
 import com.service.EmployeeServiceForHibarnateOperation;
 
 @Controller
@@ -47,6 +51,67 @@ public class ControllerForHibernateOperation {
 
 		}
 	}
+	
+	@RequestMapping(value = "/getEmployeeById", method = RequestMethod.GET)
+	public ResponseEntity<Object> getEmployeeByID(@RequestParam("id") Integer id) {
+		try {
+			logger.info("Invoking getEmployeeByID started");
+			logger.info("Controller::ControllerForHibernateOperation");
+			logger.info("Method::getEmployeeByID");
+			logger.info("HTTP Method::GET");
+			logger.info("URL::/getEmployeeByID");
+			logger.info("Arguments::" + id);
+			return new ResponseEntity<Object>(
+					employeeServiceForHibarnateOperation.processGetEmployee(id),
+					HttpStatus.OK);
+
+		} catch (MissingParameterInThePayLoad missingParameterInThePayLoad) {
+			logger.error(missingParameterInThePayLoad);
+			return new ResponseEntity<>(new ErrorResponse(
+					HttpStatus.INTERNAL_SERVER_ERROR.value(),
+					HttpStatus.INTERNAL_SERVER_ERROR.toString()),
+					HttpStatus.INTERNAL_SERVER_ERROR);
+		} catch (RuntimeException exception) {
+			logger.error(exception);
+			return new ResponseEntity<>(new ErrorResponse(
+					HttpStatus.INTERNAL_SERVER_ERROR.value(),
+					HttpStatus.INTERNAL_SERVER_ERROR.toString()),
+					HttpStatus.INTERNAL_SERVER_ERROR);
+
+		}
+	}
+	
+	@RequestMapping(value = "/saveEmployee", method = RequestMethod.POST)
+	public ResponseEntity<Object> saveEmployee(@RequestBody EmployeeEntity emp) {
+		try {
+			logger.info("Invoking saveEmployee started");
+			logger.info("Controller::ControllerForHibernateOperation");
+			logger.info("Method::saveEmployee");
+			logger.info("HTTP Method::POST");
+			logger.info("URL::/saveEmployee");
+			logger.info("Arguments::" + emp);
+			return new ResponseEntity<Object>(
+					new SuccessResponse(
+							HttpStatus.CREATED.value(), employeeServiceForHibarnateOperation.processSave(emp)),
+							HttpStatus.CREATED);
+
+
+		} catch (MissingParameterInThePayLoad missingParameterInThePayLoad) {
+			logger.error(missingParameterInThePayLoad);
+			return new ResponseEntity<>(new ErrorResponse(
+					HttpStatus.INTERNAL_SERVER_ERROR.value(),
+					HttpStatus.INTERNAL_SERVER_ERROR.toString()),
+					HttpStatus.INTERNAL_SERVER_ERROR);
+		} catch (RuntimeException exception) {
+			logger.error(exception);
+			return new ResponseEntity<>(new ErrorResponse(
+					HttpStatus.INTERNAL_SERVER_ERROR.value(),
+					HttpStatus.INTERNAL_SERVER_ERROR.toString()),
+					HttpStatus.INTERNAL_SERVER_ERROR);
+
+		}
+	}
+	
 	
 	//delete, update---vijeta
 	//getEmployee, save-- rizwan
