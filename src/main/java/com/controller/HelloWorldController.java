40f3410eace100936b83814cdeedfaa5148c6a8d
Kaustubh Damle <kaustubh.damle@ekaplus.com>
2019-06-27 09:25:05 +0530

    Added HomeController

---
+++ src/main/java/com/controller/HelloWorldController.java
@@ -0,0 +1,19 @@
+package com.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/welcome")
+public class HelloWorldController{
+ 
+	@RequestMapping(method = RequestMethod.GET)
+	public ModelAndView helloWorld(){
+ 
+		ModelAndView model = new ModelAndView("welcome");
+		model.addObject("msg", "Hello World");
+		return model;
+	}
+}
---
+++ src/main/webapp/WEB-INF/views/welcome.jsp
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Testing JSP</title>
+    </head>
+    <body>
+        <h1> ${msg}</h1>
+    </body>
+</html>
